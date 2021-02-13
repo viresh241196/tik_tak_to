7bbfece15e0ed68ac9917f3eafa32f9bd1893ddb
viresh rawool <vireshrawool007@gmail.com>
2021-02-13 11:26:58 +0530

    Added: method to display

--- TikTakTo.java
+++ TikTakTo.java
@@ -24,9 +24,19 @@ public class TikTakTo {
         System.out.println("player is " + player1 + " " + "computer is " + computer);
     }
 
+    public static void displayBoard(char[] board){
+        System.out.println(board[1]+"|"+board[2]+"|"+board[3]);
+        System.out.println("------");
+        System.out.println(board[4]+"|"+board[5]+"|"+board[6]);
+        System.out.println("------");
+        System.out.println(board[7]+"|"+board[8]+"|"+board[9]);
+        System.out.println(board);
+
+    }
     public static void main(String[] args) {
         System.out.println("welcome to TikTakTo");
-        createBoard();
-        makeChoice();
+        char[] board =createBoard();
+//        makeChoice();
+        displayBoard(board);
     }
 }
