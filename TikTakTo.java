7a730c56e96864dfe13994632b95b34cb12ea4ce
viresh rawool <vireshrawool007@gmail.com>
2021-02-13 10:00:50 +0530

    Added: a method to create board

--- TikTakTo.java
+++ TikTakTo.java
@@ -1,5 +1,13 @@
 public class TikTakTo {
-    public static void main(String[] args){
+    public static void createBoard() {
+        char[] board = new char[10];
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+    }
+
+    public static void main(String[] args) {
         System.out.println("welcome to TikTakTo");
+        createBoard();
     }
 }
